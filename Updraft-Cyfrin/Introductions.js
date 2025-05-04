4a066c515ac5fbbcc4300e8dfc6c3b790a37b453
Usman Abdullahi <ua67527056@gmail.com>
2025-05-04 23:23:49 +0100

    blockchain overview

---
+++ Updraft-Cyfrin/Introductions.js
@@ -0,0 +1,20 @@
+// Smart Contracts are a set of instructions executed in a decentralized way without the
+// need for a centralized of third party intermediary
+
+// The main difference btwn the Bitcoin Protocol and Ethereum protocol is that
+// Bitcoin is Turing Incomplete, Which means they don't have all the functionality that
+// a programming language would give them, and this was an intentional move by the bitcoing 
+// Developers
+
+
+// What is blockchain
+// Bitcoin developers viewd bitcoin as a store of value and Ethereum 
+// Developers viewed Ethereum as a store value and also a utiliy to facilitate decentralised agreement
+
+
+
+
+
+// Purpose of Smart Contract
+// A smart contract is an agreement contract, or set of instructions that is deployed
+// on a decentralized blockchain
