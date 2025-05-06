9862fdf6cc195040cecf6ce4d6aabe41c07e702f
Usman Abdullahi <ua67527056@gmail.com>
2025-05-06 15:34:07 +0100

    simple storage in solidity

---
+++ Updraft-Cyfrin/L1L2-Rollups.js
@@ -0,0 +1,20 @@
+// Layer 1: THis is the base layer of the blockchain ecosystem in which transactions
+// are executed and confirmed. This is a blockchain in it purest form. Also referred to as the 
+// the settlement layer, it is the one other chain rest on and it is the beginning, Example is BTC ETH BNB AVALANCHE
+
+// Layer 2: This is anyting built on top of the layer1
+// Defined: Secondary framework or protocols, that is built ON TOP of the Layer1
+
+// Rollup
+// Blockchain Rollup: This is a scaling solution that increases the number of 
+// transactions on the L1 chain.
+
+// The two types of Rollup are 
+// 1. Optimistic Rollup (Assumes Transactions are legitimate or valid by default) 
+// 2. ZK Rollup (Zero Knowledge): THis uses validity or ZK proof to 
+// verify transaction correctness
+
+// ZK proofs: This is an algorithm that verifies that a "prover" 
+// knows something to a "vefired" without revealing the information
+
+// One of the type of operator is Sequencer, Sequencer is responsible for ordering transactions and sometimes bundling them together
