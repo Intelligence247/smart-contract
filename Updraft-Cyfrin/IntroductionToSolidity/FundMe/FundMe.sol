912d72fbe8517e3d494f0ca9abae5ba0b7a3906a
Usman Abdullahi <ua67527056@gmail.com>
2025-05-14 14:26:56 +0100

    saved

---
+++ Updraft-Cyfrin/IntroductionToSolidity/FundMe/FundMe.sol
@@ -0,0 +1,18 @@
+// Get Funds from user
+// Withdraw Funds
+// Set a Minimun Value in USD
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract FundMe {
+    // payable keywords makes the button fund red in the UI
+    function fund() public payable {
+        // Allow Users to send $
+        // Have a minimum $ Sent
+        // 1. How do we send ETH to this contract ?
+        require(msg.value >= 1e18, "Didn't Send enough ETH"); // 1218 = 1 ETH = 1000000000000000000 wei = 1 * 10 ** 18 wei (With this people are forced to send at least 1 ETH)
+    }
+
+    // function withdraw() public {}
+}
